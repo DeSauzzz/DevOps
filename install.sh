c74d782b1f6519174eb72aad0881b929a4ce563e
DeSauzzz <adminstd@astra-live.localdomain>
2026-01-12 22:31:04 +0300

    Feat(app): Добавление Docker
     Теперь приложение можно завернуть в контейнер.
     1. Можно создать контейнер только лишь с приложением.
     Для этого достаточно запустить создание контейнера с использованием
     Dockerfile и затем внутри контейнера запустить скрипт install.sh
     2. Можно собрать и приложение, и проги для сбора метрик с помощью
     docker-compose. Пока что на работоспособность не проверялось.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update && \
+sudo apt install postgresql postgresql-contrib && \
+sudo apt install python3 && \
+sudo apt install pip && \
+sudo apt install git curl vim && \
+sudo apt install systemctl && \
+cd myapp && pip install -r requirements.txt && cd .. && \
+echo "Создание и настройка БД" && \
+service postgresql start && \
+sudo systemctl enable && sudo systemctl start && \
+sudo -u postgres psql <<EOF
+CREATE DATABASE myapp_db;
+ALTER USER postgres WITH PASSWORD 'password';
+EOF
+exit && \
+echo "База данных создана"
+
